230679d45b4a5ae14afe486b8f5ffa689cf28246
AmrAdelMohammed <Amradel525@gmail.com>
2023-09-04 12:44:12 +0300

    Initial Commit

---
+++ Seam labs Task/Seam_labs_TaskApp.swift
@@ -0,0 +1,20 @@
+//
+//  Seam_labs_TaskApp.swift
+//  Seam labs Task
+//
+//  Created by Amr Adel on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Seam_labs_TaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
